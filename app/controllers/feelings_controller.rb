0a6539140e829cb0c140302b2936747020688269
Xun Ma <lazybone1995@hotmail.com>
2019-08-12 15:50:18 +0800

    create action

--- app/controllers/feelings_controller.rb
+++ app/controllers/feelings_controller.rb
@@ -3,7 +3,25 @@
 class FeelingsController < ApplicationController
   def index
     @feelings = Feeling.all
-    @feeling = Feeling.new(user: current_user)
+    @feeling = Feeling.new
+  end
+
+  def create
+    @feeling = Feeling.new(feeling_params)
+    @feeling.user = current_user
+    if @feeling.save
+      redirect_to feelings_path
+    else
+      raise
+      redirect_to feelings_path
+      #TODO: puts error message
+    end
+  end
+
+  private
+
+  def feeling_params
+    params.require(:feeling).permit(:mood, :wifi_password, :coffee_shop, :music, :drink, :date)
   end
 
 end
