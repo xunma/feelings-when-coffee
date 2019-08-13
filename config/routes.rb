7ff7c61137f79312c2335ea1fda6cd9e3e2911c9
Xun Ma <lazybone1995@hotmail.com>
2019-08-14 01:08:47 +0800

    first version

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
 
   root to: "pages#home"
 
-  resources :feelings, only: [:index, :create]
+  resources :feelings, only: [:index, :new, :create]
 
   resources :users, only: :show
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
