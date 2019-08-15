ee0281ce2ebaad45566ecc012d434d08826afaa3
Xun Ma <lazybone1995@hotmail.com>
2019-08-16 00:19:55 +0800

    dynamically generate card

--- app/models/feeling.rb
+++ app/models/feeling.rb
@@ -2,4 +2,24 @@
 
 class Feeling < ApplicationRecord
   belongs_to :user
+
+  DETAILS = [ 'drink', 'music', 'wifi_password', 'coffee_shop']
+  DETAILS.each do |detail|
+    define_method "#{detail}_html" do
+      case detail
+      when "drink"
+        "<i class='fas fa-mug-hot'></i>  #{self.drink}" unless self.drink&.empty?
+      when "music"
+        "<i class='fas fa-music'></i>  #{self.music}" unless self.music&.empty?
+      when "coffee_shop"
+        "<i class='fas fa-store-alt'></i>  #{self.coffee_shop}" unless self.coffee_shop&.empty?
+      when "wifi_password"
+        "<i class='fas fa-wifi'></i>  #{self.wifi_password}" unless self.wifi_password&.empty?
+      end
+    end
+  end
+
+  # def #{}_html
+  #   self.drink
+  # end
 end
