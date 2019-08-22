20032c23289f58e0323c9118512066f7cfb24461
Xun Ma <lazybone1995@hotmail.com>
2019-08-22 18:13:49 +0800

    comment session done

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_20_165327) do
+ActiveRecord::Schema.define(version: 2019_08_22_094611) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "comments", force: :cascade do |t|
-    t.string "content", limit: 100
+    t.string "content", limit: 100, null: false
     t.bigint "feeling_id", null: false
     t.bigint "user_id", null: false
     t.datetime "created_at", null: false
